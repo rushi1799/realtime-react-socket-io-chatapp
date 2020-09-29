a777536f801c01a82cfaa287f2ba0d025fb440f6
unknown <rushikeshbahekar17@gmail.com>
2020-09-30 01:49:40 +0530

    server fix

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
 var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
-const rooms = ["java"];
+const rooms = [];
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
